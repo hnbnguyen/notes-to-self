ad404aa6f1eda79c177e40ea5d6ed0bc38b9063c
hnbnguyen <baohanmimi@gmail.com>
2022-10-13 16:20:12 -0400

    setting up express router

---
+++ backend/routes/items.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const router = express.Router()
+
+//GET all items
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all items'})
+})
+
+//GET a single items
+router.get('/:id', (res, req) => {
+    res.json({mssg: 'GET a single item'})
+})
+
+//POST a new item
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new item'})
+})
+
+//DELETE an item
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE an item'})
+})
+
+//UPDATE a new item
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE an item'})
+})
+
+// exporting the router with all the routes attached to it
+module.exports = router
