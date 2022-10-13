c5ff84166ef856619dc53cffc7dd2d262f0430b9
hnbnguyen <baohanmimi@gmail.com>
2022-10-13 16:54:29 -0400

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,14 @@ app.use('/api/items', itemRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("connected to mongodb & listening on port", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("listening on port", process.env.PORT)
-})
+
